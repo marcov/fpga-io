7d8e657a064c1b88877a03f502b7d0ca298f232b
Marco Vedovati <marco@kotia.local>
2013-11-13 22:14:22 +0100

    improved test for 3w + 3w fixed bug in start detection

--- threewire.v
+++ threewire.v
@@ -5,7 +5,7 @@ module threewire(input in_clk,
                  input [DATA_BITS - 1:0] in_wr_data,
                  output reg [DATA_BITS - 1:0] out_rd_data,
                  input in_start,
-                 output out_io_in_progress,
+                 output reg out_io_in_progress,
                  output out_tw_clock,
                  output reg out_tw_cs,
                  inout  io_tw_data);
@@ -21,13 +21,14 @@ module threewire(input in_clk,
     
     
     reg [2:0]  state;
-    localparam state_idle           = 0, 
-               state_txing_r_w      = 1,
-               state_txing_addr     = 2,
-               state_txing_wr_data  = 3,
-               state_rcv_prepare    = 4,
-               state_rcving_rd_data = 5,
-               state_completed      = 6;
+    localparam state_idle           = 0,
+               state_ready          = 1,
+               state_txing_r_w      = 2,
+               state_txing_addr     = 3,
+               state_txing_wr_data  = 4,
+               state_rcv_prepare    = 5,
+               state_rcving_rd_data = 6,
+               state_completed      = 7;
 
     assign io_tw_data  = io_hiz_enable ? 1'bz : tw_wr_data;
     assign out_tw_clock  = clk_enable    ? ctr_div[1] : 1'bz; 
@@ -42,100 +43,107 @@ module threewire(input in_clk,
             out_tw_cs     <= 1;
             tw_wr_data    <= 0;
             io_bits_ctr   <= 0;
+            state <=      state_idle;
+            out_io_in_progress <= 0;
         end
         else
         begin
-            ctr_div <= ctr_div + 1;
-            
-            if (ctr_div == 2'b11)
+            // state_idle is not in case block, because
+            // we must be ready to answer to in_start within 
+            // 1 in_clk cycle.
+            // case block time base is on clk divided by X.
+            if (state == state_idle)
             begin
-                case (state)
-                    state_idle:
-                    begin
-                        if (in_start)
+                if (in_start)
+                begin
+                    state <= state_ready;
+                    clk_enable <= 1;
+                end
+                else
+                begin
+                    clk_enable    <= 0;
+                    io_hiz_enable <= 0;
+                end
+            end
+            else
+            begin
+                ctr_div <= ctr_div + 1;
+                
+                if (ctr_div == 2'b11)
+                begin
+                    case (state)
+                        state_ready:
                         begin
-                            clk_enable <= 1;
                             state <= state_txing_r_w;
                         end
-                        else
+                        
+                        state_txing_r_w:
                         begin
-                            clk_enable    <= 0;
-                            io_hiz_enable <= 0;
+                            out_tw_cs <= 0;
+                            tw_wr_data  <= in_r_w;
+                            state       <= state_txing_addr;
+                            io_bits_ctr <= ADDR_BITS - 1;
                         end
-                    end
-                    
-                    state_txing_r_w:
-                    begin
-                        out_tw_cs <= 0;
-                        tw_wr_data  <= in_r_w;
-                        state       <= state_txing_addr;
-                        io_bits_ctr <= ADDR_BITS - 1;
-                    end
-                    
-                    state_txing_addr:
-                    begin
-                        if (io_bits_ctr > 0)
+                        
+                        state_txing_addr:
                         begin
-                            io_bits_ctr <= io_bits_ctr - 1;
-                        end
-                        else
-                        begin
-                            if (in_r_w == 0)
-                                state <= state_rcv_prepare;
+                            if (io_bits_ctr > 0)
+                            begin
+                                io_bits_ctr <= io_bits_ctr - 1;
+                            end
                             else
-                                state <= state_txing_wr_data;
+                            begin
+                                if (in_r_w == 0)
+                                    state <= state_rcv_prepare;
+                                else
+                                    state <= state_txing_wr_data;
+                                
+                                io_bits_ctr <= DATA_BITS - 1;
+                            end
+                            tw_wr_data  <= in_addr[io_bits_ctr];
+                        end
+                        
+                        state_rcv_prepare:
+                        begin
+                            state         <= state_rcving_rd_data;
+                            io_hiz_enable <= 1;
+                        end
+
+
+                        state_rcving_rd_data:
+                        begin
+                            if (io_bits_ctr > 0)
+                                io_bits_ctr <= io_bits_ctr - 1;
+                            else
+                                state <= state_completed;
                             
-                            io_bits_ctr <= DATA_BITS - 1;
+                            out_rd_data[io_bits_ctr] <= io_tw_data;
                         end
-                        tw_wr_data  <= in_addr[io_bits_ctr];
-                    end
-                    
-                    state_rcv_prepare:
-                    begin
-                        state         <= state_rcving_rd_data;
-                        io_hiz_enable <= 1;
-                    end
 
-
-                    state_rcving_rd_data:
-                    begin
-                        if (io_bits_ctr > 0)
+                        state_txing_wr_data:
                         begin
-                            io_bits_ctr <= io_bits_ctr - 1;
+                            if (io_bits_ctr > 0)
+                                io_bits_ctr <= io_bits_ctr - 1;
+                            else
+                                state <= state_completed;
+                            
+                            tw_wr_data  <= in_wr_data[io_bits_ctr];
                         end
-                        else
-                        begin
-                            state <= state_completed;
-                        end
-                        out_rd_data[io_bits_ctr] <= io_tw_data;
-                    end
 
-                    state_txing_wr_data:
-                    begin
-                        if (io_bits_ctr > 0)
+                        state_completed:
                         begin
-                            io_bits_ctr <= io_bits_ctr - 1;
+                            state     <= state_idle;
+                            out_tw_cs <= 1;
                         end
-                        else
+
+                        default:
                         begin
-                            state <= state_completed;
+                            /* something wrong ?? */
+                            state     <= state_idle;
                         end
-                        tw_wr_data  <= in_wr_data[io_bits_ctr];
-                    end
-
-                    state_completed:
-                    begin
-                        state     <= state_idle;
-                        out_tw_cs <= 1;
-                    end
-
-                    default:
-                    begin
-                        /* something wrong ?? */
-                        state     <= state_idle;
-                    end
-                endcase
-            end
-        end
-    end
+                    endcase
+                end //if ctr_div
+            end //if in_start ... else
+        end //if rst
+    end // always @
 endmodule
--- threewire_test.v
+++ threewire_test.v
@@ -4,31 +4,35 @@ module threewire_test;
 
     reg sim_clk;
     reg rst;
-    reg op_is_write;
+    reg iut_op_is_write;
     reg start;
-    reg [15:0]  wr_data;
-    reg [8:0]   address;
-    wire [15:0] rd_data;
-    
-    wire tw_slave_data_in;
-    reg  tw_slave_data_out;
-    reg  tw_slave_out_enable;
-
-    threewire iut_3w ( .in_clk(sim_clk),
+    reg  [15:0] iut_wr_data;
+    reg  [8:0]  iut_addr;
+    wire [15:0] iut_rd_data;
+    reg  [15:0] lt_3w_slave_tx_data; 
+    wire [8:0]  tw_slave_rcvd_addr;
+    wire [15:0] tw_slave_rcvd_data;
+    threewire iut_3w_master ( .in_clk(sim_clk),
                        .in_rst(rst),
-                       .in_r_w(op_is_write),
-                       .in_addr(address),
-                       .in_wr_data(wr_data),
-                       .out_rd_data(rd_data),
+                       .in_r_w(iut_op_is_write),
+                       .in_addr(iut_addr),
+                       .in_wr_data(iut_wr_data),
+                       .out_rd_data(iut_rd_data),
                        .in_start(start),
                        .out_io_in_progress(active),
                        .out_tw_clock(tw_bus_clock),
                        .out_tw_cs(tw_bus_chipselect),
                        .io_tw_data(tw_bus_data));
     
-    assign tw_slave_data_in = tw_bus_data;
-    assign tw_bus_data = tw_slave_out_enable ? tw_slave_data_out : 8'bz;
-    
+    tw_slave  lt_3w_slave(
+                .tw_bus_clock(tw_bus_clock),
+                .tw_bus_chipselect(tw_bus_chipselect),
+                .tw_bus_data(tw_bus_data),
+                .rcvd_addr(tw_slave_rcvd_addr),
+                .rcvd_data(tw_slave_rcvd_data),
+                .tx_data(lt_3w_slave_tx_data),
+                .rcvd_mode_w(tw_slave_mode_w));
+
     initial begin
         #0
         $dumpfile("test.lxt");
@@ -37,78 +41,114 @@ module threewire_test;
         sim_clk = 0;
         rst = 0;
         start = 0;
-        tw_slave_out_enable = 0;
-        tw_slave_data_out = 0;
 
-        #75
+        #25
         rst = 1;
 
-        #100
+        #25
         rst = 0;
 
-        #150
-        start       = 1;
-        address     = 9'h155;
-        wr_data     = 16'hAA;
-        op_is_write = 0;
+        #25
+        ///////// TEST READ //////
+        start           = 1;
+        iut_addr        = 9'h155;
+        iut_op_is_write = 0;
         //since iut is reading, we have to feed the slave emulator with a value.
-        tw_slave_transmit_data = 16'hCA;
-
-        #165
+        lt_3w_slave_tx_data = 16'h96CA;
+        #15
         start = 0;
-        
+       
+
+        #2000
+        ///////// TEST WRITE //////
+        start           = 1;
+        iut_addr        = 9'h1B2;
+        iut_wr_data     = 16'hA4F9;
+        iut_op_is_write = 1;
+        #15
+        start = 0;
+       
 
         #10000
         $finish;
     end
 
 
-    reg tw_slave_mode_is_write;
-    reg [iut_3w.ADDR_BITS - 1 : 0] tw_slave_rcvd_address;
-    reg [iut_3w.DATA_BITS - 1 : 0] tw_slave_incoming_data;
-    reg [iut_3w.DATA_BITS - 1 : 0] tw_slave_transmit_data;
-    reg [127 : 0] i;
 
+
+    //66MHz
+	always #7.5 sim_clk = !sim_clk;
+endmodule
+
+
+module tw_slave(tw_bus_clock,
+                tw_bus_chipselect,
+                tw_bus_data,
+                rcvd_addr,
+                rcvd_data,
+                tx_data,
+                rcvd_mode_w);
+
+    parameter ADDR_BITS = 9;
+    parameter DATA_BITS = 16;
+    
+    input tw_bus_clock;
+    input tw_bus_chipselect;
+    inout tw_bus_data;
+    output reg rcvd_mode_w;
+    output reg [ADDR_BITS - 1 : 0] rcvd_addr;
+    output reg [DATA_BITS - 1 : 0] rcvd_data;
+    input  [DATA_BITS - 1 : 0] tx_data;
+    
+    reg [127 : 0] i;
+    wire tw_slave_data_in;
+    reg  tw_slave_data_out;
+    reg  tw_slave_out_enable;
+    
+    
+    assign tw_slave_data_in = tw_bus_data;
+    assign tw_bus_data = tw_slave_out_enable ? tw_slave_data_out : 8'bz;
+     
     always
     begin : slave_emulator
+        
+        // STARTS HERE
+        tw_slave_out_enable = 0;
+        tw_slave_data_out   = 0;
+        
         wait (tw_bus_chipselect == 0)
         
-        @(posedge tw_bus_clock) tw_slave_mode_is_write = !tw_slave_data_in;
+        @(posedge tw_bus_clock) rcvd_mode_w = !tw_slave_data_in;
             
-        for (i = 0; i < iut_3w.ADDR_BITS; i = i + 1)
+        for (i = 0; i < ADDR_BITS; i = i + 1)
         begin
-            @(posedge tw_bus_clock) tw_slave_rcvd_address[iut_3w.ADDR_BITS - 1 - i] = tw_slave_data_in; 
+            @(posedge tw_bus_clock) rcvd_addr[ADDR_BITS - 1 - i] = tw_slave_data_in; 
         end
 
-        if (!tw_slave_mode_is_write)
+        if (!rcvd_mode_w)
         begin
             // just read
-            for (i = 0; i < iut_3w.DATA_BITS; i = i + 1)
+            for (i = 0; i < DATA_BITS; i = i + 1)
             begin
-                @(posedge tw_bus_clock) tw_slave_incoming_data[iut_3w.DATA_BITS - 1 - i] = tw_slave_data_in; 
+                @(posedge tw_bus_clock) rcvd_data[DATA_BITS - 1 - i] = tw_slave_data_in; 
             end
         end
         else
         begin
             //we must write
-            @ (posedge tw_bus_clock) tw_slave_out_enable = 1;
 
-            for (i = 0; i < iut_3w.DATA_BITS; i = i + 1)
+            for (i = 0; i < DATA_BITS; i = i + 1)
             begin
-                @(posedge tw_bus_clock)  tw_slave_data_out =  tw_slave_transmit_data[iut_3w.DATA_BITS - 1 - i];
+                @(posedge tw_bus_clock)  tw_slave_data_out =  tx_data[DATA_BITS - 1 - i];
+                if (i == 0)
+                    tw_slave_out_enable = 1;
             end
             
             @ (posedge tw_bus_clock) tw_slave_out_enable = 0;
         end
 
         wait (tw_bus_chipselect == 1) 
-            // FAKE instruction
+            // fake instruction
             i = i;
     end
-
-
-    //66MHz
-	always #7.5 sim_clk = !sim_clk;
-
 endmodule
-
