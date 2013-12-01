55a40410af1fa069f7c02a46f4d7cc854ed7dbb6
Marco Vedovati <marco@kotia.local>
2013-12-01 01:10:19 +0100

    ft2232h emu

--- top_test.v
+++ top_test.v
@@ -27,100 +27,129 @@ module top_test;
 	// Inputs
 	reg sim_clk;
 	reg in_reset_n;
-	reg in_ftdi_rxf_n;
-	reg in_ftdi_txe_n;
 
 	// Outputs
 	wire out_led;
 	wire out_ftdi_wr_n;
 	wire out_ftdi_rd_n;
 
-
-
-    wire [7:0] t_ftdi_io_data;
-
-    wire [7:0] _iobuffer_in;
-    wire [7:0] _iobuffer_out;
-    reg        t_ftdi_emu_out_enable;
-    reg [7:0]  t_ftdi_emu_tx_data;
-    reg [7:0]  t_reg_in_data;
-
-    assign _iobuffer_in     = t_ftdi_io_data;
-    assign t_ftdi_io_data   = t_ftdi_emu_out_enable ? _iobuffer_out : 8'bz;
-    assign _iobuffer_out    = t_ftdi_emu_tx_data;
-	 
-	 
-	// Instantiate the Unit Under Test (UUT)
+    wire [7:0] io_ftdi_data;
+	
+    // Instantiate the Unit Under Test (UUT)
 	top uut (
 		.in_ext_osc(sim_clk), 
 		.in_reset_n(in_reset_n), 
 		.out_led(out_led), 
-		.io_ftdi_data(t_ftdi_io_data), 
+		.io_ftdi_data(io_ftdi_data), 
 		.in_ftdi_rxf_n(in_ftdi_rxf_n), 
 		.in_ftdi_txe_n(in_ftdi_txe_n), 
 		.out_ftdi_wr_n(out_ftdi_wr_n), 
 		.out_ftdi_rd_n(out_ftdi_rd_n)
 	);
 
-    // HERE we are emulating the FT2332H!
-	initial begin
+    ft2232h_device ft2232h_lt(
+         .in_rd_n (out_ftdi_rd_n),
+         .in_wr_n (out_ftdi_wr_n),
+         .out_txe_n (in_ftdi_txe_n),
+         .out_rxf_n (in_ftdi_rxf_n),
+         .io_data   (io_ftdi_data));
+          
+
+     initial
+     begin
 		// Initialize Inputs
 		#0
 		sim_clk = 0;
 		in_reset_n = 1;
-		in_ftdi_rxf_n = 1;
-		in_ftdi_txe_n = 1;
-		t_ftdi_emu_out_enable = 0;
 
 		#10
 		in_reset_n = 0;
 		
 		#50
 		in_reset_n = 1;
-
-		#100
-		t_ftdi_emu_out_enable = 1;
-		t_ftdi_emu_tx_data   = 16'h00;
-		in_ftdi_rxf_n   = 0;
 		
-		#150
-		in_ftdi_txe_n = 0;
-		
-		// Wait 100 ns for global reset to finish
+        // Wait 100 ns for global reset to finish
 		#10000;
-		
-       
-		// Add stimulus here 
-	end
-	
+     end
+
     //66MHz
 	always #7.5 sim_clk = !sim_clk; 
-	
-	/*
-	 always @ (posedge out_ftdi_rd_n)
-    begin
-        in_ftdi_rxf_n          <= 1;
-        t_ftdi_emu_out_enable        <= 0;
-    end
-
-    always @ (posedge out_ftdi_wr_n)
-    begin
-        in_ftdi_txe_n <= 1;
-    end
-    */ 
-	 
-	 
-	 
-    always @ (negedge out_ftdi_rd_n)
-    begin
-        t_ftdi_emu_out_enable        <= 1;
-    end
-	 
-	always @ (posedge out_ftdi_rd_n)
-    begin
-        t_ftdi_emu_out_enable        <= 0;
-        //t_ftdi_emu_tx_data <= t_ftdi_emu_tx_data + 1;
-    end
-	 
 endmodule
 
+////////////////////////////////////////////////////////
+
+module ft2232h_device(input in_rd_n,
+                      input in_wr_n,
+                      output reg out_txe_n,
+                      output reg out_rxf_n,
+                      inout  [7:0] io_data,
+                      input  [7:0] usb_tx_data,
+                      output reg [7:0] usb_rx_data);
+
+    wire [7:0] in_data;
+    reg  [7:0] out_data;
+    reg        io_out_enable;
+
+    reg [15:0] rcvd_data;
+
+    assign in_data   = io_data;
+    assign io_data   = io_out_enable ? out_data : 8'bz;
+    
+    // HERE we are emulating the FT2332H!
+	always 
+    begin : ft2232h_emulator
+
+        // Set io to input
+		io_out_enable = 0;
+        
+		// Disable FPGA -> USB 
+        out_txe_n         = 1;
+
+        //Enable USB -> FPGA
+        out_rxf_n         = 0;
+
+        // TX char
+	    wait(in_rd_n == 0);
+		io_out_enable = 1;
+		out_data    = 8'h00;
+	    wait(in_rd_n == 1);
+		io_out_enable = 0;
+
+        // TX char
+	    wait(in_rd_n == 0);
+		io_out_enable = 1;
+		out_data    = 8'h01;
+	    wait(in_rd_n == 1);
+		io_out_enable = 0;
+
+        // TX char
+	    wait(in_rd_n == 0);
+		io_out_enable = 1;
+		out_data    = 8'h5A;
+	    wait(in_rd_n == 1);
+		io_out_enable = 0;
+        
+        //Disable USB -> FPGA
+        out_rxf_n         = 1;
+		
+		// Enable FPGA -> USB 
+        out_txe_n         = 0;
+	    
+        rcvd_data = 0;
+
+        // RX char
+        wait(in_wr_n == 0);
+        rcvd_data = rcvd_data | (in_data << 8);
+        wait(in_wr_n == 1);
+		// Enable FPGA -> USB 
+        out_txe_n         = 0;
+
+        // RX char
+        wait(in_wr_n == 0);
+        rcvd_data = rcvd_data | in_data;
+        wait(in_wr_n == 1);
+		// Disable FPGA -> USB 
+        out_txe_n         = 1;
+
+	end
+endmodule
