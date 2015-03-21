3c114153be416767b3d34329e62d1d24f4dd4b90
Marco Vedovati <marco@kotia.local>
2015-03-21 09:51:15 +0100

    add checks

--- src/io_bitbang/io_bitbang_test.v
+++ src/io_bitbang/io_bitbang_test.v
@@ -19,18 +19,17 @@ module threewire_testbench;
     reg  [IUT_IO_NUM_OF - 1 : 0] iut_outval;
     
     inout  [IUT_IO_NUM_OF - 1 : 0] model_pins;
-    reg    [IUT_IO_NUM_OF - 1 : 0] lt_pads_drive_val;
+    reg    [IUT_IO_NUM_OF - 1 : 0] lt_outval;
     
     wire [IUT_IO_NUM_OF - 1 : 0] lt_direction;
 
-    assign lt_direction = ~ iut_direction;
+    assign lt_direction = iut_direction ^ 'h3FF;
 
-    lt_pad lt_pads[IUT_IO_NUM_OF - 1 : 0] (lt_direction, lt_pads_drive_val, model_pins);
+    lt_pad lt_pads[IUT_IO_NUM_OF - 1 : 0] (lt_direction, lt_outval, model_pins);
                     
-    io_bitbang io_bitbang_inst(
-                        .in_io_direction(iut_direction),
-                        .in_io_outval(iut_outval),
-                        .io_pins(model_pins));
+    io_bitbang io_bitbang_inst(.in_io_direction(iut_direction),
+                               .in_io_outval(iut_outval),
+                               .io_pins(model_pins));
 
     initial begin
         #0
@@ -38,44 +37,111 @@ module threewire_testbench;
         $dumpvars(0, threewire_testbench);
         sim_clk = 0;
         rst = 0;
-        
         //////////////////////////
         #25
         rst = 1;
         #25
         rst = 0;
 
+        #5
         iut_direction = 'h3FF;
         iut_outval    = 'h2AA;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
 
         #5
         iut_outval    = 'h155;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
 
         #5
         iut_direction = 'h0;
-        lt_pads_drive_val    = 'h3A5;
-        $display("Model pins set to input, value = %x", model_pins);
+        lt_outval    = 'h3A5;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
        
         #5
-        lt_pads_drive_val    = 'h244;
-        $display("Model pins set to input, value = %x", model_pins);
+        lt_outval    = 'h244;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
         
         #5
-        lt_pads_drive_val    = 'bz;
-        $display("Model pins set to input, value = %x", model_pins);
+        lt_outval    = 'bz;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
         
         #5
         iut_outval              = 'h3FF;
         iut_direction           = 'h255;
-        lt_pads_drive_val    = 'h3A5;
-        $display("Model pins set to input, value = %x", model_pins);
+        lt_outval    = 'h3A5;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
         
         #5
         iut_outval              = 'h0;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
         
         #5
-        lt_pads_drive_val    = 'h3FF;
-        $display("Model pins set to input, value = %x", model_pins);
+        lt_outval    = 'h3FF;
+        #1
+        $display("%d: PINS val=%b - IUT dir=%b outval=%b - LT dir=%b outval=%b", 
+                 $time, model_pins, iut_direction, iut_outval, lt_direction, lt_outval);
+        if (((iut_outval & iut_direction) !== (model_pins & iut_direction)) ||
+            ((lt_outval & lt_direction) !== (model_pins & lt_direction)) )
+        begin
+            $display(">>>> FAIL!");
+            $finish;
+        end
         
         #10000
         $finish;
